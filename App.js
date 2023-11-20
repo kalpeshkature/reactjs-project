89b1ac3632c12bd6e28915a073f5f461156c14ac
kalpeshkature <130071074+kalpeshkature@users.noreply.github.com>
2023-11-20 22:21:47 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+  const name='Kalpesh'
+  return (
+    <div className="container">
+      
+      <Header title='Hello'/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
